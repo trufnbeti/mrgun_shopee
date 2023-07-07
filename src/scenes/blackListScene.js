f24796f5911e89f8d5f038b1b5c4fa68eefa26f8
Atis <ntlena99@gmail.com>
2023-07-07 11:04:25 +0700

    add ui black list scene

--- src/scenes/blackListScene.js
+++ src/scenes/blackListScene.js
@@ -1,19 +1,87 @@
-import { Container, Graphics } from "pixi.js";
+import * as PIXI from "pixi.js";
+import {
+  Application,
+  Assets,
+  Container,
+  Graphics,
+  RenderTexture,
+  Sprite,
+} from "pixi.js";
 import { GameConstant } from "../gameConstant";
 
 export class BlackListScene extends Container{
 
     constructor() {
         super();
-        // Add black background
-        const background = new Graphics();
-        background.beginFill(0x202020);
-        background.drawRect(0, 0, GameConstant.GAME_WIDTH, GameConstant.GAME_HEIGHT);
-        background.endFill();
-        this.addChild(background);
+        this._initBackGround();
+        this._initTextBig();
+        this._initTextSmall();
+        this._initTextTitle();
+        this._initBGListBoss();
+        this._iniTextList();
 
         console.log("black list scene");
         
+    }
+
+    _iniTextList(){
+        let levelBlackList = 1;
+        this.textList = new PIXI.Text("LIST NO. 1", this.smallTextStyle);
+        this.textList.anchor.set(0.5);
+        this.textList.zIndex = 2;
+        this.textList.position.set(GameConstant.GAME_WIDTH/2, (GameConstant.GAME_HEIGHT - 900) / 2 + 50);
+        this.addChild(this.textList);
+    }
+
+    _initBGListBoss() {
+        this.bgListBoss = new Graphics();
+        this.bgListBoss.beginFill(0x293B4B);
+        
+        const width = 600;
+        const height = 900;
+        const x = (GameConstant.GAME_WIDTH - width) / 2;
+        const y = (GameConstant.GAME_HEIGHT - height) / 2;
+        const cornerRadius = 30; // Điều chỉnh bán kính bo góc tròn tại đây
+        
+        this.bgListBoss.drawRoundedRect(x, y, width, height, cornerRadius);
+        
+        this.bgListBoss.endFill();
+        this.addChild(this.bgListBoss);
+    }
+
+
+    _initTextTitle() {
+        this.textTitle = new PIXI.Text("BLACK LIST", this.bigTextStyle);
+        this.textTitle.anchor.set(0.5);
+        this.textTitle.zIndex = 2;
+        this.textTitle.position.set(GameConstant.GAME_WIDTH/2, 80);
+        this.addChild(this.textTitle);
+      }
+
+    _initTextBig() {
+        this.bigTextStyle = new PIXI.TextStyle({
+          fontFamily: "Century Gothic",
+          fontSize: 35,
+          // fontStyle: 'italic',
+          fontWeight: "bold",
+          fill: ["#ffffff"],
+        });
+      }
+
+      _initTextSmall() {
+        this.smallTextStyle = new PIXI.TextStyle({
+          fontFamily: "Helvetica",
+          fontSize: 25,
+          fill: ["#ffffff"],
+        });
+    }
+    _initBackGround(){
+        // Add black background
+        const background = new Graphics();
+        background.beginFill(0x131313);
+        background.drawRect(0, 0, GameConstant.GAME_WIDTH, GameConstant.GAME_HEIGHT);
+        background.endFill();
+        this.addChild(background);
     }
 
     
